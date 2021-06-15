93f27e743cd476f082e8df59a86e311611dd6f14
Kornelius Septa Jasa Sipayung <writecodex09@gmail.com>
2021-06-15 09:36:03 +0700

    update bug
    update bug distribusi barang ke ekspedisi dan endpoint

--- src/main.js
+++ src/main.js
@@ -39,10 +39,10 @@ export async function configure(aurelia) {
       var master = "https://com-bateeq-service-core-dev.azurewebsites.net/v1/";
       var manufacture ="https://com-bateeq-service-core-dev.azurewebsites.net/v1/";
       // var inventory ="https://com-bateeq-service-warehouse-dev.azurewebsites.net/v1/";
-      var inventory ="http://localhost:53273/v1/";
+      var inventory ="https://com-efrata-service-warehouse-dev.azurewebsites.net/v1/";
       var inventoryAzure = "https://com-bateeq-service-inventory-dev.azurewebsites.net/v1/"
       // var merchandiser ="https://com-bateeq-service-warehouse-dev.azurewebsites.net/v1/";
-      var merchandiser ="http://localhost:53273/v1/";
+      var merchandiser ="https://com-efrata-service-warehouse-dev.azurewebsites.net/v1/";
       //var merchandiser = "http://localhost:51011/v1/";
       //var md ="http://localhost:61542/v1/";
       var md ="https://com-bateeq-service-merchandiser-dev.azurewebsites.net/v1/";
@@ -52,7 +52,7 @@ export async function configure(aurelia) {
       // var sales = "http://localhost:63614/v1/";
 
       // var purchasing ="https://com-bateeq-service-warehouse-dev.azurewebsites.net/v1/";
-      var purchasing ="http://localhost:53273/v1/";
+      var purchasing ="https://com-efrata-service-purchasingjob-dev.azurewebsites.net/v1/";
       // var purchasingAzure = "https://com-bateeq-service-purchasing-dev.azurewebsites.net/v1/";
       var purchasingAzure = "https://com-efrata-service-purchasingjob-dev.azurewebsites.net/v1/";
       // var purchasingAzure = "http://localhost:58205/v1/";
