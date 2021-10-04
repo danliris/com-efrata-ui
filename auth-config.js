b3599ffffc7950e3e1a2c24a181ea35296b80701
danliris <danliris.cloud@gmail.com>
2021-10-04 15:53:15 +0700

    update

--- auth-config.js
+++ auth-config.js
@@ -2,7 +2,7 @@ export default {
     endpoint: "auth",
     configureEndpoints: ["auth", "core", "master", "manufacture", "inventory", "inventory-azure", "merchandiser", "md", "sales", "purchasing", "purchasing-azure", "finance", "ncore", "nmasterplan", "nsales","nmerchandiser", "purchasingJob","garment-production"],
 
-    loginUrl: "authenticate",
+    loginUrl: "/authenticate",
     profileUrl: "/me",
 
     authTokenType:"Bearer",
